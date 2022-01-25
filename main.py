376e32445ddcc2962b2477781229a596862cbac1
HaakonSvane <haakonsvane@gmail.com>
2022-01-25 16:11:36 +0100

    rework README.md

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import math
-
+from typing import Tuple, List
 
 # Note that the function only works for integers that satisfy 2^k, where k is any natural number.
 # See README for more info on this.
 
-def func(parts):
-    orig_k = math.log2(parts)
+def eliminationPermute(parts: int) -> List[Tuple]:
+    orig_k = int(math.log2(parts))
+    matches = []
 
-    def rec(i, p):
-        a = p
-        b = parts / (2 ** (orig_k - i)) + 1 - p
+    def rec(i: int, p: int) -> None:
+        a = 2 ** i + 1 - p
         if i == orig_k:
-            print("%s\t\t-\t\t%s" % (int(a), int(parts + 1 - a)))
-            return
+            # print("%s\t\t-\t\t%s" % (int(p), int(parts + 1 - p)))
+            matches.append((p, parts + 1 - p))
         else:
+            rec(i + 1, p)
             rec(i + 1, a)
-            rec(i + 1, b)
 
     rec(1, 1)
+    return matches
